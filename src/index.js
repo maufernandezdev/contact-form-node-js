023a381bed1e976c46c8bdf32a3a7f77a9f350b1
Mauricio Fernandez <mfernandezuk@hotmail.com>
2022-06-12 20:27:48 -0300

    vercel app json is added

--- src/index.js
+++ src/index.js
@@ -7,9 +7,3 @@ app.use(bodyParser.urlencoded({ extended: false })); // parse application/x-www-
 
 app.use(require('./routes/index'));
 app.use(express.static(path.join(__dirname, 'public' )));
-
-app.listen(5500,() =>{
-
-    console.log("server on port 5500");
-
-});
